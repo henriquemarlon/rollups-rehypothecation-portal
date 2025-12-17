2420ac0ae7acc90f8334fa21e674e4755da47266
Henrique Santos <henriquemarloncs@gmail.com>
2025-12-17 03:29:07 -0300

    add dependencies

--- src/ERC20ReHypothecationPortal.sol
+++ src/ERC20ReHypothecationPortal.sol
@@ -3,18 +3,20 @@
 
 pragma solidity ^0.8.8;
 
+import {Currency} from "v4-core/types/Currency.sol";
 import {IERC20} from "@openzeppelin-contracts-5.2.0/token/ERC20/IERC20.sol";
-
-import {InputEncoding} from "cartesi-rollups-contracts-2.1.1/src/common/InputEncoding.sol";
+import {Portal} from "cartesi-rollups-contracts-2.1.1/src/portals/Portal.sol";
 import {IInputBox} from "cartesi-rollups-contracts-2.1.1/src/inputs/IInputBox.sol";
 import {IERC20Portal} from "cartesi-rollups-contracts-2.1.1/src/portals/IERC20Portal.sol";
-import {Portal} from "cartesi-rollups-contracts-2.1.1/src/portals/Portal.sol";
+import {InputEncoding} from "cartesi-rollups-contracts-2.1.1/src/common/InputEncoding.sol";
+
+/// referece: https://github.com/OpenZeppelin/uniswap-hooks/blob/master/src/general/ReHypothecationHook.sol
 
 /// @title ERC-20 Portal
-///
+/// 
 /// @notice This contract allows anyone to perform transfers of
 /// ERC-20 tokens to an application contract while informing the off-chain machine.
-contract ERC20Portal is IERC20Portal, Portal {
+contract ERC20ReHypothecationPortal is IERC20Portal, Portal {
     /// @notice Constructs the portal.
     /// @param inputBox The input box used by the portal
     constructor(IInputBox inputBox) Portal(inputBox) {}
@@ -36,4 +38,36 @@ contract ERC20Portal is IERC20Portal, Portal {
 
         getInputBox().addInput(appContract, payload);
     }
+
+    //     /**
+    //  * @dev Returns the `yieldSource` address for a given `currency`.
+    //  *
+    //  * Note: Must be implemented and adapted for the desired type of yield sources, such as
+    //  *  ERC-4626 Vaults, or any custom DeFi protocol interface, optionally handling native currency.
+    //  */
+    // function getCurrencyYieldSource(Currency currency) public view virtual returns (address yieldSource);
+
+    // /**
+    //  * @dev Deposits a specified `amount` of `currency` into its corresponding yield source.
+    //  *
+    //  * Note: Must be implemented and adapted for the desired type of yield sources, such as
+    //  *  ERC-4626 Vaults, or any custom DeFi protocol interface, optionally handling native currency.
+    //  */
+    // function _depositToYieldSource(Currency currency, uint256 amount) internal virtual;
+
+    // /**
+    //  * @dev Withdraws a specified `amount` of `currency` from its corresponding yield source.
+    //  *
+    //  * Note: Must be implemented and adapted for the desired type of yield sources, such as
+    //  *  ERC-4626 Vaults, or any custom DeFi protocol interface, optionally handling native currency.
+    //  */
+    // function _withdrawFromYieldSource(Currency currency, uint256 amount) internal virtual;
+
+    // /**
+    //  * @dev Gets the `amount` of `currency` deposited in its corresponding yield source.
+    //  *
+    //  * Note: Must be implemented and adapted for the desired type of yield sources, such as
+    //  *  ERC-4626 Vaults, or any custom DeFi protocol interface, optionally handling native currency.
+    //  */
+    // function _getAmountInYieldSource(Currency currency) internal view virtual returns (uint256 amount);
 }
